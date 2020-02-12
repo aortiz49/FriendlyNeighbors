a26e4d9373f871684d0c8d280d97037f17018ec7
Carlos Figueredo <ce.figueredo@uniandes.edu.co>
2020-02-12 12:12:19 -0500

    Entity

--- s3_neighborhood-back/src/main/java/co/edu/uniandes/csw/neighborhood/entities/LoginEntity.java
+++ s3_neighborhood-back/src/main/java/co/edu/uniandes/csw/neighborhood/entities/LoginEntity.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package co.edu.uniandes.csw.vecindario.entities;
 
 import java.io.Serializable;
@@ -56,5 +55,6 @@ public void setIdLogin(int pId){
     public void setRol(String rol) {
         this.rol = rol;
     }
-
+ 
+    
 }
