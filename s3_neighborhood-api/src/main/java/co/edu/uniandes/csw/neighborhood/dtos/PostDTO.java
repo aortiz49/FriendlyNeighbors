6818b7a6bfc51d45e63de34f2d199a26a064e075
Andres Bayona <al.bayona@uniandes.edu.co>
2020-02-26 22:23:21 -0500

    Andres Bayona DTOs

--- s3_neighborhood-api/src/main/java/co/edu/uniandes/csw/neighborhood/dtos/PostDTO.java
+++ s3_neighborhood-api/src/main/java/co/edu/uniandes/csw/neighborhood/dtos/PostDTO.java
@@ -7,6 +7,10 @@ import co.edu.uniandes.csw.neighborhood.entities.PostEntity;
 * @generated
 */
 public class PostDTO {
+
+    PostDTO(PostEntity entityPost) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
   
 
