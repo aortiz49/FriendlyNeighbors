e07af54d0b2bdefa168c27cb48737c64cdce1858
aortiz49 <a.ortizg@uniandes.edu.co>
2020-03-10 13:27:00 -0500

    Add resource

--- s3_neighborhood-api/src/main/java/co/edu/uniandes/csw/neighborhood/resources/ResidentProfileResource.java
+++ s3_neighborhood-api/src/main/java/co/edu/uniandes/csw/neighborhood/resources/ResidentProfileResource.java
@@ -19,7 +19,6 @@ import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import static javax.ws.rs.HttpMethod.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
