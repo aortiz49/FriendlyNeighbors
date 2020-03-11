ba64715b6747e1f936c39b1e75f43df7186a2f4e
Kevin Romero <k.romero@uniandes.edu.co>
2020-03-11 08:23:58 -0500

    event resource and Service resource

--- s3_neighborhood-api/src/main/java/co/edu/uniandes/csw/neighborhood/dtos/EventDTO.java
+++ s3_neighborhood-api/src/main/java/co/edu/uniandes/csw/neighborhood/dtos/EventDTO.java
@@ -1,21 +1,187 @@
 package co.edu.uniandes.csw.neighborhood.dtos;
 
+import co.edu.uniandes.csw.neighborhood.adapters.DateAdapter;
 import co.edu.uniandes.csw.neighborhood.entities.EventEntity;
+import co.edu.uniandes.csw.neighborhood.entities.EventEntity;
+import co.edu.uniandes.csw.neighborhood.entities.LocationEntity;
+import co.edu.uniandes.csw.neighborhood.entities.ResidentProfileEntity;
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.ManyToOne;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import uk.co.jemos.podam.common.PodamExclude;
 
 
 /**
-* @generated
+* @k.romero
 */
-public class EventDTO {
+public class EventDTO implements Serializable{
+/**
+     * The title of the event.
+     */
+   private String title;
 
-    public EventDTO(EventEntity entity) {
-    }
-
-    EventEntity toEntity() {
-        return new EventEntity();
-    }
+   /**
+     * Represents the date  was created
+     */
+    @XmlJavaTypeAdapter(DateAdapter.class)
+    private Date datePosted;
     
+    
+    
+    
+    /**
+     * Represents the date group was created
+     */
+    @XmlJavaTypeAdapter(DateAdapter.class)
+    private Date dateOfEvent;
+    
+    
+    
+    /**
+     * The start time of the event.
+     */
+    private String startTime;
 
+    /**
+     * The end time of the event.
+     */
+    private String endTime;
+
+    /**
+     * The description of the event.
+     */
+    private String description;
+
+    /**
+     * The availability of the event. How long the event will take place.
+     */
+    private String availability;
+    /**
+     * The host of the event.
+     */
+     private ResidentProfileDTO host;
+     /**
+     * The location of the event.
+     */
+    
+    private LocationDTO location;
+//===================================================
+// Getters & Setters
+//===================================================
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public Date getDatePosted() {
+        return datePosted;
+    }
+
+    public void setDatePosted(Date datePosted) {
+        this.datePosted = datePosted;
+    }
+
+    public Date getDateOfEvent() {
+        return dateOfEvent;
+    }
+
+    public void setDateOfEvent(Date dateOfEvent) {
+        this.dateOfEvent = dateOfEvent;
+    }
+
+    public ResidentProfileDTO getHost() {
+        return host;
+    }
+
+    public void setHost(ResidentProfileDTO host) {
+        this.host = host;
+    }
+
+    public LocationDTO getLocation() {
+        return location;
+    }
+
+    public void setLocation(LocationDTO location) {
+        this.location = location;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(String availability) {
+        this.availability = availability;
+    }
+
+    public EventDTO(){super();}
+    
+   
+    public EventDTO(EventEntity entityEvent) {
+        this.availability = entityEvent.getAvailability();
+        this.description = entityEvent.getDescription();
+        this.endTime = entityEvent.getEndTime();
+        this.startTime = entityEvent.getStartTime();
+        this.title =  entityEvent.getTitle();
+        if (entityEvent.getLocation()!= null) {
+            this.location = new LocationDTO(entityEvent.getLocation());
+        }
+        if (entityEvent.getHost()!= null) {
+            this.host = new ResidentProfileDTO(entityEvent.getHost());
+        }
+    }
+
+    /**
+     * Converts a event DTO to a event entity.
+     *
+     * @return new event entity
+     *
+     */
+    public EventEntity toEntity() {
+        EventEntity eventEntity = new EventEntity();
+
+        eventEntity.setAvailability(getAvailability());
+        eventEntity.setDescription(getDescription());
+        eventEntity.setEndTime(getEndTime());
+        eventEntity.setStartTime(getStartTime());
+        eventEntity.setTitle(getTitle());    
+
+         if (location != null) {
+            eventEntity.setLocation(getLocation().toEntity());
+        }
+         
+          if (host != null) {
+            eventEntity.setHost(getHost().toEntity());
+        }
+        return eventEntity;
+    }
     
     
 }
--- s3_neighborhood-api/src/main/java/co/edu/uniandes/csw/neighborhood/dtos/PostDTO.java
+++ s3_neighborhood-api/src/main/java/co/edu/uniandes/csw/neighborhood/dtos/PostDTO.java
@@ -1,23 +1,168 @@
 package co.edu.uniandes.csw.neighborhood.dtos;
 
+import co.edu.uniandes.csw.neighborhood.adapters.DateAdapter;
+import co.edu.uniandes.csw.neighborhood.entities.BusinessEntity;
+import co.edu.uniandes.csw.neighborhood.entities.GroupEntity;
+import co.edu.uniandes.csw.neighborhood.entities.NeighborhoodEntity;
 import co.edu.uniandes.csw.neighborhood.entities.PostEntity;
+import co.edu.uniandes.csw.neighborhood.entities.ResidentProfileEntity;
+import co.edu.uniandes.csw.neighborhood.podam.DateStrategy;
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import uk.co.jemos.podam.common.PodamExclude;
+import uk.co.jemos.podam.common.PodamStrategyValue;
 
 
 /**
-* @generated
+* @k.romero
 */
-public class PostDTO {
+public class PostDTO implements Serializable{
+    /**
+     * Represents the date post was made
+     */
+    @XmlJavaTypeAdapter(DateAdapter.class)
+    private Date publishDate;
+    /**
+     * Represents the title of the post
+     */
+    private String title;
 
-    PostDTO(PostEntity entityPost) {
-       
-    }
+    /**
+     * Represents the main text body shown in the post
+     */
+    private String description;
     
+    /**
+     * Represents the number of numberOfLikes of the post
+     */
+    private Integer numberOfLikes;
+
+    
+    /**
+     * The neighborhood to which the group belongs to.
+     */
+    
+     /**
+     * Represents the author of this post 
+     */
+    
+    private ResidentProfileDTO author;
+
+    /**
+     * Represents the business author of this post 
+     */
+    
+    private BusinessDTO business;
+    
+    public Date getPublishDate() {
+        return publishDate;
+    }
+
+      /**
+     * Represents the group this post is shared with
+     */
   
-
-    PostEntity toEntity() {
-         return new PostEntity();
+   private GroupDTO group;
+    public void setPublishDate(Date publishDate) {
+        this.publishDate = publishDate;
     }
-    
+
+    public GroupDTO getGroup() {
+        return group;
+    }
+
+    public void setGroup(GroupDTO group) {
+        this.group = group;
+    }
+
+    public ResidentProfileDTO getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(ResidentProfileDTO author) {
+        this.author = author;
+    }
+
+    public BusinessDTO getBusiness() {
+        return business;
+    }
+
+    public void setBusiness(BusinessDTO business) {
+        this.business = business;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getNumberOfLikes() {
+        return numberOfLikes;
+    }
+
+    public void setNumberOfLikes(Integer numberOfLikes) {
+        this.numberOfLikes = numberOfLikes;
+    }
+    public PostDTO(){super();}
+  
+    public PostDTO(PostEntity entityPost) {
+        
+        this.description = entityPost.getDescription();
+        
+        this.numberOfLikes = entityPost.getNumberOfLikes();
+        this.title =  entityPost.getTitle();
+        if (entityPost.getAuthor()!= null) {
+            this.author = new ResidentProfileDTO(entityPost.getAuthor());
+        }
+        if (entityPost.getBusiness()!= null) {
+            this.business = new BusinessDTO(entityPost.getBusiness());
+        }
+        if (entityPost.getGroup()!= null) {
+            this.group = new GroupDTO(entityPost.getGroup());
+        }
+    }
+
+    /**
+     * Converts a post DTO to a post entity.
+     *
+     * @return new post entity
+     *
+     */
+    public PostEntity toEntity() {
+        PostEntity postEntity = new PostEntity();
+
+        
+        postEntity.setDescription(getDescription());
+       
+        postEntity.setNumberOfLikes(getNumberOfLikes());
+        postEntity.setTitle(getTitle());  
+        if (author != null) {
+            postEntity.setAuthor(getAuthor().toEntity());
+        }
+        if (business != null) {
+            postEntity.setBusiness(getBusiness().toEntity());
+        }
+        if (group != null) {
+            postEntity.setGroup(getGroup().toEntity());
+        }
+
+        return postEntity;
+    }
     
     
     
