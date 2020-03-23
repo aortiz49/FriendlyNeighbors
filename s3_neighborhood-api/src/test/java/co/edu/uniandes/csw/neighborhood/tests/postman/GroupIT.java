74b818c8fd72bdb03f614c6bb055b4a40e2de0b0
Andres Bayona <al.bayona@uniandes.edu.co>
2020-03-23 14:35:21 -0500

    Postman

--- s3_neighborhood-api/src/test/java/co/edu/uniandes/csw/neighborhood/tests/postman/GroupIT.java
+++ s3_neighborhood-api/src/test/java/co/edu/uniandes/csw/neighborhood/tests/postman/GroupIT.java
@@ -29,7 +29,7 @@ import org.junit.runner.RunWith;
 @RunWith(Arquillian.class)
 public class GroupIT {
     
-    private static final String COLLECTION = "Group-Tests.postman_collection";
+     private static final String COLLECTION = "Group-Tests.postman_collection";
 
     @Deployment(testable = true)
     public static WebArchive createDeployment() {
